5e869bab0dab206c8c3222bc247b516335779776
Brian Ball <Brian.Ball@nrel.gov>
2017-09-21 09:36:58 -0600

    rgenoud debug

--- server/lib/analysis_library/rgenoud.rb
+++ server/lib/analysis_library/rgenoud.rb
@@ -268,7 +268,10 @@ class AnalysisLibrary::Rgenoud < AnalysisLibrary::Base
             rails_host = "#{APP_CONFIG['os_server_host_url']}"
             r_scripts_path = "#{APP_CONFIG['r_scripts_path']}"
             rails_exit_guideline_14 = "#{@analysis.exit_on_guideline_14}"
-            source(paste(r_scripts_path,'/rgenoud.R',sep=''))
+            call_rgenoud <- try(source(paste(r_scripts_path,'/rgenoud.R',sep='')))
+            print(paste("inherit try-error:",inherits(call_rgenoud,'try-error')))
+            print(paste("class call_rgenoud:",class(call_rgenoud)))
+            print(paste("call_rgenoud:",call_rgenoud))
             print("finished rgenoud.R script")
           }
         end
@@ -278,8 +281,9 @@ class AnalysisLibrary::Rgenoud < AnalysisLibrary::Base
         raise 'could not start the cluster (most likely timed out)'
       end
 
-    rescue => e
+    rescue SecurityError, ScriptError, NoMemoryError, SignalException, StandardError => e
       log_message = "#{__FILE__} failed with #{e.message}, #{e.backtrace.join("\n")}"
+      logger.info(log_message)
       logger.error log_message
       @analysis.status_message = log_message
       @analysis.save!
