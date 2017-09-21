447e471f3ac72bfc00e57ce7c21b939345110a17
Brian Ball <Brian.Ball@nrel.gov>
2017-09-21 12:03:51 -0600

    try latest Rserve

--- docker/R/install_packages.R
+++ docker/R/install_packages.R
@@ -1,4 +1,4 @@
-q# Install Packages
+# Install Packages
 install.packages('lhs', repos=c('http://cloud.r-project.org','http://cran.r-project.org'))
 install.packages('e1071', repos=c('http://cloud.r-project.org','http://cran.r-project.org'))
 install.packages('triangle', repos=c('http://cloud.r-project.org','http://cran.r-project.org'))
@@ -20,4 +20,4 @@ install.packages('Rcpp', repos=c('http://cloud.r-project.org','http://cran.r-pro
 install.packages('plyr', repos=c('http://cloud.r-project.org','http://cran.r-project.org'))
 install.packages('ggplot2', repos=c('http://cloud.r-project.org','http://cran.r-project.org'))
 install.packages('sensitivity', repos=c('http://cloud.r-project.org','http://cran.r-project.org'))
-install.packages('Rserve', configure.args=c('PKG_CPPFLAGS=-DNODAEMON'), repos=c('http://cloud.r-project.org','http://cran.r-project.org'))
+install.packages('Rserve', configure.args=c('PKG_CPPFLAGS=-DNODAEMON'), repos=c('http://rforge.net'))
